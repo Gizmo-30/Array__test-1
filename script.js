c251c3666707e70fa88ebe0174f3b3e44554c8d6
Gizmo-30 <anvaradizov80@gmail.com>
2021-12-10 12:47:53 +0500

    first

---
+++ script.js
@@ -0,0 +1,16 @@
+let num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+for (let i = 0; i < num.length; i++) {
+    if (num[i] % 2 == 0) {
+        var a = num[i]
+        console.log(`ЧЕТНОЕ ЧИСЛО ${num[i]}`);
+    }
+}
+for (let i = 0; i < num.length; i++) {
+    if (num[i] % 2 > 0) {
+        var и = num[i]
+        console.log(`НЕЧЕТНОЕ ЧИСЛО ${num[i]}`);
+    }
+}
+
+
